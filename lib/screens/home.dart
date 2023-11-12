6785693644dfd8a18fc1dd39a9cfff525e9f022e
Jorge Henriques <jorge.cavalcanti2@gmail.com>
2023-11-12 16:01:04 -0300

    fix: add menu bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:map_for_events/models/emalta.dart';
 import 'package:map_for_events/models/eventos.dart';
+import 'package:map_for_events/screens/login.dart';
+import 'package:map_for_events/utils/contants.dart';
+
 class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
 
@@ -14,16 +17,27 @@ class HomeWidget extends StatefulWidget {
 
 class _HomeWidgetState extends State<HomeWidget> {
   List<Eventos> eventos = [
-    Eventos('nome', 'descricao', 'imagem', 'data', 'local', 'horario', 'valor', 'link', 'categoria')
+    Eventos('nome', 'descricao', 'imagem', 'data', 'local', 'horario', 'valor',
+        'link', 'categoria')
   ];
-  
+
   List<EmAlta> emAlta = [
-  EmAlta('CCXP 2023', 'Maior festival de cultura pop do planeta', 'assets/ccxp.jpg'),
-  EmAlta('Estética in São Paulo', 'Participe do evento mais importante de estética, saúde', 'assets/esteticainsaopaulo.png'),
-  EmAlta('Hospitalar 2024', 'A maior Feira da América Latina Efe', 'assets/hospitalar.jpg'),
-  EmAlta('Anime Friends 2024', 'O maior evento de cultura pop da América', 'assets/animefriends.jpg'),
-  EmAlta('Brasil Game Show 2024', 'A Maior Feira de Games da América Latina', 'assets/bgs.jpeg'),
-  EmAlta('REATECH BRASIL 2024', 'Feira Internacional de Inclusão e Acessibilidade', 'assets/reatech.jpg'),
+    EmAlta('CCXP 2023', 'Maior festival de cultura pop do planeta',
+        'assets/ccxp.jpg'),
+    EmAlta(
+        'Estética in São Paulo',
+        'Participe do evento mais importante de estética, saúde',
+        'assets/esteticainsaopaulo.png'),
+    EmAlta('Hospitalar 2024', 'A maior Feira da América Latina Efe',
+        'assets/hospitalar.jpg'),
+    EmAlta('Anime Friends 2024', 'O maior evento de cultura pop da América',
+        'assets/animefriends.jpg'),
+    EmAlta('Brasil Game Show 2024', 'A Maior Feira de Games da América Latina',
+        'assets/bgs.jpeg'),
+    EmAlta(
+        'REATECH BRASIL 2024',
+        'Feira Internacional de Inclusão e Acessibilidade',
+        'assets/reatech.jpg'),
   ];
 
   @override
@@ -43,16 +57,16 @@ class _HomeWidgetState extends State<HomeWidget> {
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: 0,
-        items: const [
-          BottomNavigationBarItem(
-              icon: iconHome, label: 'Home', backgroundColor: colorMainBlue),
-          BottomNavigationBarItem(icon: iconSearch, label: 'Procurar'),
-          BottomNavigationBarItem(icon: iconTicket, label: 'Eventos'),
-          BottomNavigationBarItem(icon: iconFavorite, label: 'Favoritos'),
-          BottomNavigationBarItem(icon: iconUser, label: 'Perfil'),
-        ],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: 0,
+          items: const [
+            BottomNavigationBarItem(
+                icon: iconHome, label: 'Home', backgroundColor: colorMainBlue),
+            BottomNavigationBarItem(icon: iconSearch, label: 'Procurar'),
+            BottomNavigationBarItem(icon: iconTicket, label: 'Eventos'),
+            BottomNavigationBarItem(icon: iconFavorite, label: 'Favoritos'),
+            BottomNavigationBarItem(icon: iconUser, label: 'Perfil'),
+          ]),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -66,29 +80,29 @@ class _HomeWidgetState extends State<HomeWidget> {
                   fit: BoxFit.cover,
                 ),
                 Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Icon(Icons.gamepad, color: Colors.white),
-                            Text('Geek, Games, Pop', style: TextStyle(color: Colors.white)),
-                          ],
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                            'CCXP 2024',
-                            style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                            
-                          ),
-                        ),
+                        Icon(Icons.gamepad, color: Colors.white),
+                        Text('Geek, Games, Pop',
+                            style: TextStyle(color: Colors.white)),
                       ],
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'CCXP 2024',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ],
                 ),
+              ],
+            ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
@@ -122,14 +136,17 @@ class _HomeWidgetState extends State<HomeWidget> {
                               fit: BoxFit.cover,
                             ),
                             SizedBox(height: 8),
-                            Text(emAlta[index].title, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-                            Text(emAlta[index].subtitulo, style: TextStyle(fontSize: 11, color: Colors.grey)),
+                            Text(emAlta[index].title,
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold)),
+                            Text(emAlta[index].subtitulo,
+                                style: TextStyle(
+                                    fontSize: 11, color: Colors.grey)),
                           ],
                         ),
                       ),
                     ),
                   ),
-
                   Text(
                     'Gastronomia e Culinária',
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
@@ -158,15 +175,17 @@ class _HomeWidgetState extends State<HomeWidget> {
                               fit: BoxFit.cover,
                             ),
                             SizedBox(height: 8),
-                            Text(emAlta[index].title, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-                            Text(emAlta[index].subtitulo, style: TextStyle(fontSize: 11, color: Colors.grey)),
+                            Text(emAlta[index].title,
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold)),
+                            Text(emAlta[index].subtitulo,
+                                style: TextStyle(
+                                    fontSize: 11, color: Colors.grey)),
                           ],
                         ),
                       ),
                     ),
                   ),
-
-
                   Text(
                     'Geek e Games',
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
@@ -195,15 +214,17 @@ class _HomeWidgetState extends State<HomeWidget> {
                               fit: BoxFit.cover,
                             ),
                             SizedBox(height: 8),
-                            Text(emAlta[index].title, style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-                            Text(emAlta[index].subtitulo, style: TextStyle(fontSize: 11, color: Colors.grey)),
+                            Text(emAlta[index].title,
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.bold)),
+                            Text(emAlta[index].subtitulo,
+                                style: TextStyle(
+                                    fontSize: 11, color: Colors.grey)),
                           ],
                         ),
                       ),
                     ),
                   ),
-
-
                 ],
               ),
             ),
